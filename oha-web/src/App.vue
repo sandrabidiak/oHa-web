6d17b64f9382c550812f4f93358dc4ca36fe6486
Sandra Bigyak <sandra.bidiak@gmail.com>
2018-10-22 22:53:08 +0200

    - initial project set-up

---
+++ oha-web/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <SearchPage msg="Welcome to oha-web app"/>
+  </div>
+</template>
+
+<script>
+import SearchPage from './components/SearchPage.vue';
+
+export default {
+  name: 'app',
+  components: {
+    SearchPage,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
