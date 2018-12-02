1853c9e7e5761c3bcf9437663ea429dba809f0e7
Sandra Bigyak <sandra.bidiak@gmail.com>
2018-12-02 17:19:00 +0100

    - added styles to the DetailPage - added initial carousel

--- oha-web/src/components/detail/DetailPage.vue
+++ oha-web/src/components/detail/DetailPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="full-page">
     <div v-if="selectedResult">
       <section class="container-fluid h-100">
         <section class="row cancel-row">
@@ -7,30 +7,51 @@
             <router-link :to="{name: 'Search'}">
               <button type="button" class="btn back-btn-style">Back</button>
             </router-link>
-          </section>    
+          </section>
         </section>
-        <section class="row detail-row">
-          <section class="col-md-5">
-            <div>
-              <h4 class="text-center">{{selectedResult.ti}}</h4>
-              <p v-html="selectedResult.sd"></p>
-            </div>
-          </section>
-          <section class="col-md-7">
-            <div>
-              {{"Image"}}
-            </div>     
-          </section>
-          <section class="col-12">
-            <div class="border-top divider my-4"></div>
-          </section>
-          <section class="col">
-            <div v-html="selectedResult.ld"></div>
-            <div>
-              <a v-bind:href="selectedResult.ur" target="_blank">Home Page</a>
-            </div>
-            <div v-html="selectedResult.es"></div>
-            <div class="footer footer-ref" v-html="selectedResult.cr"></div>
+        <section class="row mt-4 detail-row">
+          <section class="col align-self-center">
+            <section class="row justify-content-center align-items-center">
+              <section class="col-md-4">
+                <div class="text-left">
+                  <h4>{{selectedResult.ti}}</h4>
+                  <p v-html="selectedResult.sd"></p>
+                  <button v-if="selectedResult.ur" class="btn home-page-btn">
+                    <a class="invisible-link"
+                      v-bind:href="selectedResult.ur" target="_blank">Home Page
+                    </a>
+                  </button>
+                </div>
+              </section>
+              <section class="col-md-6">
+                <div id="imageCarousel" class="carousel slide" data-ride="carousel">
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img class="d-block w-100" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" alt="Second slide">
+                    </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#imageCarousel" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#imageCarousel" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
+              </section>
+              <section class="col-md-10">
+                <div class="border-top divider my-4"></div>
+              </section>
+              <section class="col-md-10">
+                <div v-html="selectedResult.ld"></div>
+                <div v-html="selectedResult.es"></div>
+                <div class="footer footer-ref" v-html="selectedResult.cr"></div>
+              </section>
+            </section>
           </section>
         </section>
       </section>
@@ -47,7 +68,7 @@ export default {
   computed: {
     selectedResult() {
       const id = this.$route.params.id;
-      if(!this.$store.state.results.fa){
+      if (!this.$store.state.results.fa) {
         return undefined;
       }
       return this.$store.state.results.fa.find(r => r.rid === id);
