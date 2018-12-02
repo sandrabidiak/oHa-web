50ba02f84719d49a5aa0b97313084627f08a8752
Sandra Bigyak <sandra.bidiak@gmail.com>
2018-12-02 18:12:04 +0100

    - added working carousel to DetailPage

--- oha-web/src/components/detail/DetailPage.vue
+++ oha-web/src/components/detail/DetailPage.vue
@@ -24,24 +24,12 @@
                 </div>
               </section>
               <section class="col-md-6">
-                <div id="imageCarousel" class="carousel slide" data-ride="carousel">
-                  <div class="carousel-inner">
-                    <div class="carousel-item active">
-                      <img class="d-block w-100" alt="First slide">
-                    </div>
-                    <div class="carousel-item">
-                      <img class="d-block w-100" alt="Second slide">
-                    </div>
-                  </div>
-                  <a class="carousel-control-prev" href="#imageCarousel" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                  </a>
-                  <a class="carousel-control-next" href="#imageCarousel" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                  </a>
-                </div>
+                 <b-carousel id="imageCarousel" controls indicators :interval="0">
+                    <b-carousel-slide v-for="(image,index) in selectedResult.im" :key ="index" 
+                      class="carousel-item">
+                      <img slot="img" class="d-block img-fluid w-100" v-bind:src="image.im">
+                    </b-carousel-slide>
+                 </b-carousel>
               </section>
               <section class="col-md-10">
                 <div class="border-top divider my-4"></div>
