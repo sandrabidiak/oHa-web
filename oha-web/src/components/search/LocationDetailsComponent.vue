f0befe53d7c082c2280e6ba751c365dc17a1b65d
Sandra Bigyak <sandra.bidiak@gmail.com>
2018-12-07 22:09:47 +0100

    - added language button to the nav-bar - moved slider to the SearchComponent

--- oha-web/src/components/search/LocationDetailsComponent.vue
+++ oha-web/src/components/search/LocationDetailsComponent.vue
@@ -2,18 +2,28 @@
   <div>
     <section class="container-fluid">
       <section class="row location-detail-style">
-        <div class="col-12 col-lg-7 justify-content-sm-center mt-2">
-          <i class="fa fa-location-arrow mr-2"></i>  
+        <div class="col-12 col-sm-7 justify-content-sm-center my-2">
+          <i class="fa fa-location-arrow mr-2"></i>
           {{getLocationName()}}
           <router-link :to="{name: 'Locations'}">
             <button class="btn btn-sm change-location-btn ml-4">{{ $t('change_location') }}</button>
           </router-link>
         </div>
-        <div class="col-sm-8 col-lg-5 justify-self-xs-center mt-2">
-          <span class="mr-2">{{ $t('radius') }}: {{sliderValue}} km</span>
-          <b-form-input @change="onSliderChange($event)" :value="sliderValue" type="range"
-            min="1" max="25" step="1">
-          </b-form-input>
+        <div class="col-2 offset-sm-3">
+          <div class="btn-group btn-group-toggle my-2" data-toggle="buttons">
+            <label class="btn btn-sm"
+              v-bind:class="{ 'nav-language-btn-style-checked': $i18n.locale === 'en',
+              'nav-language-btn-style': $i18n.locale !== 'en'}">
+              <input @click="selectLanguage('en')"
+                type="radio" name="language" id="english" autocomplete="off" checked>EN
+            </label>
+            <label class="btn btn-sm"
+              v-bind:class="{ 'nav-language-btn-style-checked': $i18n.locale === 'de',
+              'nav-language-btn-style': $i18n.locale !== 'de'}">
+              <input @click="selectLanguage('de')"
+                type="radio" name="language" id="german" autocomplete="off">DE
+            </label>
+          </div>
         </div>
       </section>
     </section>
@@ -27,9 +37,6 @@ export default {
     location() {
       return this.$store.state.selectedLocation;
     },
-    sliderValue() {
-      return this.$store.state.selectedSliderValue;
-    },
   },
   data() {
     return {};
@@ -38,8 +45,9 @@ export default {
     getLocationName() {
       return this.location ? this.location.name : 'No location selected!';
     },
-    onSliderChange(value) {
-      this.$store.commit('setSliderValue', value);
+    selectLanguage(language) {
+      this.$i18n.locale = language;
+      this.$store.commit('setSelectedLanguage', language);
     },
   },
 };
--- oha-web/src/components/search/SearchComponent.vue
+++ oha-web/src/components/search/SearchComponent.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="full-page">
     <section class="container-fluid">
+      <section class="row justify-content-sm-center">
+        <div class="col-10 col-md-5">
+          <span class="mr-2">{{ $t('radius') }}: {{sliderValue}} km</span>
+          <b-form-input @change="onSliderChange($event)" :value="sliderValue" type="range"
+            min="1" max="25" step="1">
+          </b-form-input>
+        </div>
+      </section>
       <section class="row justify-content-md-center">
-        <div class="col-9 col-md-6">
+        <div class="col-8 col-md-6">
           <vue-tags-input class="input-custom tag-custom" v-model="tag" :tags="tags"
             @tags-changed="newTags => onTagsChange(newTags)"/>
         </div>
-        <div class="col-3 col-md-2">
+        <div class="col-2 col-md-2">
           <button class="btn search-btn-style" @click="search()">{{ $t('search') }}</button>
         </div>
       </section>
@@ -26,6 +34,9 @@ export default {
     tags() {
       return this.$store.state.inputTags;
     },
+    sliderValue() {
+      return this.$store.state.selectedSliderValue;
+    },
   },
   props: {
     msg: String,
@@ -43,6 +54,9 @@ export default {
     },
     onTagsChange(tags) {
       this.$store.commit('setInputTags', tags);
+    },
+    onSliderChange(value) {
+      this.$store.commit('setSliderValue', value);
     },
   },
 };
